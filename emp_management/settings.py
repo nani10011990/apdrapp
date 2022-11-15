8a882fcb3b7ace2b63d4a199d28283a3dbc0583a
unknown <saveweavingandfarmers@gmail.com>
2022-11-15 12:32:27 +0530

    first commit

--- emp_management/settings.py
+++ emp_management/settings.py
@@ -55,9 +55,9 @@ ALLOWED_HOSTS = ['*']
 # CORS_ALLOWED_ORIGINS = [
 #     'http://localhost:4200',
 # ]
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:4200',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:4200',
+# ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
